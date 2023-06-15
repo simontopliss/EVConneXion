f7b08d3907d455870c7a57fd8c6732f18a1e424d
simontopliss <simontopliss@me.com>
2023-06-15 13:30:01 +0100

    Initial Commit

---
+++ EVChargePoints/EVChargePointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVChargePointsApp.swift
+//  EVChargePoints
+//
+//  Created by Simon Topliss on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct EVChargePointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
