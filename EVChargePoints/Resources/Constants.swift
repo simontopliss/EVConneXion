93b9b3af82648937ec1c5ccc10110f10a968c060
simontopliss <simontopliss@me.com>
2023-06-29 15:44:40 +0100

    Create Constants file

---
+++ EVChargePoints/Resources/Constants.swift
@@ -0,0 +1,45 @@
+//
+//  Constants.swift
+//  EVChargePoints
+//
+//  Created by Simon Topliss on 29/06/2023.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum Tab: Int {
+        case map
+        case list
+        case routes
+        case settings
+        
+        var label: String {
+            switch self {
+                case .map:
+                    return "Map"
+                case .list:
+                    return "List"
+                case .routes:
+                    return "Routes"
+                case .settings:
+                    return "Settings"
+            }
+        }
+        
+        var icon: String {
+            switch self {
+                case .map:
+                    return "map"
+                case .list:
+                    return "list.bullet"
+                case .routes:
+                    return "arrow.triangle.swap"
+                case .settings:
+                    return "gearshape"
+            }
+        }
+    }
+    
+}
