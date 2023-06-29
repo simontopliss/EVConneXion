3b6599c867721d4834167ece422350499f5965fe
simontopliss <simontopliss@me.com>
2023-06-29 17:35:19 +0100

    Create ChargePointDataTests and check test_zero

---
+++ EVChargePointsTests/ChargePointDataTests.swift
@@ -0,0 +1,9 @@
+@testable import EVChargePoints
+import XCTest
+
+@MainActor
+final class ChargePointDataTests: XCTestCase {
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in ChargePointDataTests")
+    }
+}
