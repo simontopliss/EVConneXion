30f51f79a263559f304fa0eec1f4fa8cc5b3c0b6
simontopliss <simontopliss@me.com>
2023-06-29 15:41:03 +0100

    Created Unit Tests

---
+++ EVChargePointsTests/EVChargePointsTests.swift
@@ -0,0 +1,22 @@
+//
+//  EVChargePointsTests.swift
+//  EVChargePointsTests
+//
+//  Created by Simon Topliss on 29/06/2023.
+//
+
+import XCTest
+
+final class EVChargePointsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+
+}
