78888b973eeb95d59547eac20a13d4b4bbab2ab8
simontopliss <simontopliss@me.com>
2023-06-29 17:19:06 +0100

    Create TabModel and UserDefaultKeys

---
+++ EVChargePoints/Base/Model/TabModel.swift
@@ -0,0 +1,42 @@
+//
+//  TabModel.swift
+//  EVChargePoints
+//
+//  Created by Simon Topliss on 29/06/2023.
+//
+
+import Foundation
+
+enum TabModel: Int {
+
+    case map
+    case list
+    case routes
+    case settings
+
+    var label: String {
+        switch self {
+            case .map:
+                return "Map"
+            case .list:
+                return "List"
+            case .routes:
+                return "Routes"
+            case .settings:
+                return "Settings"
+        }
+    }
+
+    var icon: String {
+        switch self {
+            case .map:
+                return "map"
+            case .list:
+                return "list.bullet"
+            case .routes:
+                return "arrow.triangle.swap"
+            case .settings:
+                return "gearshape"
+        }
+    }
+}
---
+++ EVChargePoints/Base/Model/UserDefaultKeys.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaultKeys.swift
+//  EVChargePoints
+//
+//  Created by Simon Topliss on 29/06/2023.
+//
+
+import Foundation
+
+enum UserDefaultKeys {
+    static let country       = "country"
+    static let distance      = "distance"
+    static let limit         = "limit"
+    static let tabSelection  = "tabSelection"
+    static let units         = "units"
+}
