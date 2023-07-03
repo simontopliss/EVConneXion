3e9b6f778540bd50afc5da9e72da927f885b1e18
simontopliss <simontopliss@me.com>
2023-07-03 17:09:38 +0100

    Fix for deviceAccess not parsing correctly in all cases

--- EVChargePoints/Base/Model/ChargePointData.swift
+++ EVChargePoints/Base/Model/ChargePointData.swift
@@ -8,11 +8,9 @@
 
 import Foundation
 
-// MARK: - ChargePointData
-
-public struct ChargePointData: Decodable {
+struct ChargePointData: Decodable {
     let scheme: Scheme
-    let chargeDevices: [ChargeDevice]
+    var chargeDevices: [ChargeDevice]
 
     enum CodingKeys: String, CodingKey {
         case scheme = "Scheme"
@@ -22,47 +20,47 @@ public struct ChargePointData: Decodable {
 
 // MARK: - ChargeDevice
 
-public struct ChargeDevice: Decodable {
-    let chargeDeviceId: String
-    let chargeDeviceRef: String
-    let chargeDeviceName: String
-    let chargeDeviceText: String?
-    let chargeDeviceLocation: ChargeDeviceLocation
-    let chargeDeviceModel: String?
-    let publishStatusId: String
-    let dateCreated: String
-    let dateUpdated: String
-    let attribution: String
-    let dateDeleted: String
-    let connector: [Connector]
-    let deviceOwner: DeviceOwner
-    let deviceController: DeviceController
-    let deviceAccess: DeviceAccess
-    let deviceNetworks: String
-    let chargeDeviceStatus: ChargeStatus
-    let publishStatus: String
-    let deviceValidated: String
-    let recordModerated: RecordModerated
-    let recordLastUpdated: String?
-    let recordLastUpdatedBy: String?
-    let paymentRequiredFlag: Bool
-    let paymentDetails: String?
-    let subscriptionRequiredFlag: Bool
-    let subscriptionDetails: String?
-    let parkingFeesFlag: Bool
-    let parkingFeesDetails: String?
-    let parkingFeesUrl: String?
-    let accessRestrictionFlag: Bool
-    let accessRestrictionDetails: String?
-    let physicalRestrictionFlag: Bool
-    let physicalRestrictionText: String?
-    let onStreetFlag: Bool
-    let locationType: LocationType
-    let bearing: String?
-    let accessible24Hours: Bool
+struct ChargeDevice: Decodable {
+    var chargeDeviceID: String
+    var chargeDeviceRef: String
+    var chargeDeviceName: String
+    var chargeDeviceText: String?
+    var chargeDeviceLocation: ChargeDeviceLocation
+    var chargeDeviceModel: String?
+    var publishStatusId: String
+    var dateCreated: String
+    var dateUpdated: String
+    var attribution: String
+    var dateDeleted: String
+    var connector: [Connector]
+    var deviceOwner: DeviceOwner
+    var deviceController: DeviceController
+    var deviceAccess: DeviceAccess?
+    var deviceNetworks: String
+    var chargeDeviceStatus: ChargeStatus
+    var publishStatus: String
+    var deviceValidated: String
+    var recordModerated: RecordModerated
+    var recordLastUpdated: String?
+    var recordLastUpdatedBy: String?
+    var paymentRequiredFlag: Bool
+    var paymentDetails: String?
+    var subscriptionRequiredFlag: Bool
+    var subscriptionDetails: String?
+    var parkingFeesFlag: Bool
+    var parkingFeesDetails: String?
+    var parkingFeesUrl: String?
+    var accessRestrictionFlag: Bool
+    var accessRestrictionDetails: String?
+    var physicalRestrictionFlag: Bool
+    var physicalRestrictionText: String?
+    var onStreetFlag: Bool
+    var locationType: LocationType
+    var bearing: String?
+    var accessible24Hours: Bool
 
     enum CodingKeys: String, CodingKey {
-        case chargeDeviceId = "ChargeDeviceId"
+        case chargeDeviceID = "ChargeDeviceId"
         case chargeDeviceRef = "ChargeDeviceRef"
         case chargeDeviceName = "ChargeDeviceName"
         case chargeDeviceText = "ChargeDeviceText"
@@ -124,7 +122,7 @@ enum LocationType: String, Decodable, CaseIterable {
 // This seems to be the solution to catch `unknown` types
 // https://stackoverflow.com/a/49697266/7429227
 extension LocationType {
-    public init(from decoder: Decoder) throws {
+    init(from decoder: Decoder) throws {
         self = try LocationType(
             rawValue: decoder.singleValueContainer().decode(RawValue.self)
         ) ?? .unknown
@@ -134,11 +132,11 @@ extension LocationType {
 // MARK: - ChargeDeviceLocation
 
 struct ChargeDeviceLocation: Decodable {
-    let latitude: String
-    let longitude: String
-    let address: [String: String?]
-    let locationShortDescription: String?
-    let locationLongDescription: String?
+    var latitude: String
+    var longitude: String
+    var address: [String: String?]
+    var locationShortDescription: String?
+    var locationLongDescription: String?
 
     enum CodingKeys: String, CodingKey {
         case latitude = "Latitude"
@@ -157,17 +155,17 @@ enum ChargeStatus: String, Decodable {
 // MARK: - Connector
 
 struct Connector: Decodable {
-    let connectorId: String
-    let connectorType: ConnectorType
-    let ratedOutputkW: String
-    let ratedOutputVoltage: String
-    let ratedOutputCurrent: String
-    let chargeMethod: ChargeMethod
-    let chargeMode: String
-    let chargePointStatus: ChargeStatus
-    let tetheredCable: TetheredCable
-    let information: String?
-    let validated: String
+    var connectorId: String
+    var connectorType: ConnectorType
+    var ratedOutputkW: String
+    var ratedOutputVoltage: String
+    var ratedOutputCurrent: String
+    var chargeMethod: ChargeMethod
+    var chargeMode: String
+    var chargePointStatus: ChargeStatus
+    var tetheredCable: TetheredCable
+    var information: String?
+    var validated: String
 
     enum CodingKeys: String, CodingKey {
         case connectorId = "ConnectorId"
@@ -222,31 +220,86 @@ enum ConnectorTypeID: Int {
 // MARK: - DeviceAccess
 
 struct DeviceAccess: Decodable {
-    let open24Hours: Bool
+    var open24Hours: Bool = false
+    var regularOpenings: [RegularOpening]?
 
     enum CodingKeys: String, CodingKey {
         case open24Hours = "Open24Hours"
+        case regularOpenings = "RegularOpenings"
     }
 }
 
 extension DeviceAccess {
-    public init(from decoder: Decoder) throws {
+    init(from decoder: Decoder) throws {
         do {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             open24Hours = try container.decode(Bool.self, forKey: .open24Hours)
         } catch {
             open24Hours = false
         }
+
+        do {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            regularOpenings = try container.decodeIfPresent([RegularOpening].self, forKey: .regularOpenings)
+        } catch {
+            print(error)
+            regularOpenings = nil
+        }
+    }
+}
+
+// MARK: - RegularOpening
+
+struct RegularOpening: Codable {
+    var days: String?
+    var hours: Hours?
+    var allDays = false
+
+    enum CodingKeys: String, CodingKey {
+        case days = "Days"
+        case hours = "Hours"
+        case allDays
+    }
+}
+
+extension RegularOpening {
+    init(from decoder: Decoder) throws {
+        do {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            days = try container.decode(String.self, forKey: .days)
+        } catch {
+            days = nil
+            allDays = true
+        }
+
+        do {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            hours = try container.decode(Hours.self, forKey: .hours)
+        } catch {
+            hours = nil
+        }
+    }
+}
+
+// MARK: - Hours
+
+struct Hours: Codable {
+    var from: String
+    var to: String
+
+    enum CodingKeys: String, CodingKey {
+        case from = "From"
+        case to = "To"
     }
 }
 
 // MARK: - DeviceController
 
 struct DeviceController: Decodable {
-    let organisationName: String
-    let schemeCode: String
-    let website: String
-    let telephoneNo: String
+    var organisationName: String
+    var schemeCode: String
+    var website: String
+    var telephoneNo: String
 
     enum CodingKeys: String, CodingKey {
         case organisationName = "OrganisationName"
@@ -259,10 +312,10 @@ struct DeviceController: Decodable {
 // MARK: - DeviceOwner
 
 struct DeviceOwner: Decodable {
-    let organisationName: String
-    let schemeCode: String
-    let website: String
-    let telephoneNo: String
+    var organisationName: String
+    var schemeCode: String
+    var website: String
+    var telephoneNo: String
 
     enum CodingKeys: String, CodingKey {
         case organisationName = "OrganisationName"
@@ -280,8 +333,8 @@ enum RecordModerated: String, Decodable {
 // MARK: - Scheme
 
 struct Scheme: Decodable {
-    let schemeCode: String
-    let schemeData: SchemeData
+    var schemeCode: String
+    var schemeData: SchemeData
 
     enum CodingKeys: String, CodingKey {
         case schemeCode = "SchemeCode"
@@ -292,9 +345,9 @@ struct Scheme: Decodable {
 // MARK: - SchemeData
 
 struct SchemeData: Decodable {
-    let organisationName: String
-    let website: String
-    let telephoneNo: String
+    var organisationName: String
+    var website: String
+    var telephoneNo: String
 
     enum CodingKeys: String, CodingKey {
         case organisationName = "OrganisationName"
