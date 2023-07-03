8e939098650cc520539893297c72a666e935357b
simontopliss <simontopliss@me.com>
2023-07-03 18:09:20 +0100

    Add mocks for UI testing

---
+++ EVChargePoints/Mocks/NetworkManagerFailureMock.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManagerFailureMock.swift
+//  iOSTakeHomeProjectTests
+//
+//  Created by Simon Topliss on 30/06/2023.
+//
+
+#if DEBUG
+
+import Foundation
+
+final class NetworkManagerFailureMock: NetworkManagerImpl {
+    func request<T>(_ baseURL: String, type: T.Type) async throws -> T where T : Decodable {
+        throw NetworkManager.NetworkError.invalidURL
+    }
+}
+
+#endif
---
+++ EVChargePoints/Mocks/NetworkManagerSuccessMock.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkingManagerCreateSuccessMock.swift
+//  iOSTakeHomeProjectTests
+//
+//  Created by Simon Topliss on 30/06/2023.
+//
+
+#if DEBUG
+
+import Foundation
+
+final class NetworkManagerSuccessMock: NetworkManagerImpl {
+    func request<T>(_ baseURL: String, type: T.Type) async throws -> T where T : Decodable {
+        return Data() as! T // swiftlint:disable:this force_cast
+    }
+}
+
+#endif
