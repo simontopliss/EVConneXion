4991d6c6799751b905a3e4de2178cdf468b5558b
simontopliss <simontopliss@me.com>
2023-07-03 11:39:08 +0100

    Change DecodeJSON.decode to static func and delete comments

--- EVChargePoints/Base/API/DecodeJSON.swift
+++ EVChargePoints/Base/API/DecodeJSON.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum DecodeJSON {
 
-    func decode<T: Decodable>(
+    static func decode<T: Decodable>(
         data: Data,
         type _: T.Type,
         dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
@@ -23,19 +23,25 @@ enum DecodeJSON {
         do {
             return try decoder.decode(T.self, from: data)
         } catch let DecodingError.keyNotFound(key, context) {
-            // fatalError("Failed to decode due to missing key '\(key.stringValue)' – \(context.debugDescription)")
-            throw DecodeJSONError.keyNotFound("Failed to decode due to missing key '\(key.stringValue)' – \(context.debugDescription)")
+            throw DecodeJSONError.keyNotFound(
+                error: "Failed to decode due to missing key '\(key.stringValue)' – \(context.debugDescription)" as! Error
+            )
         } catch let DecodingError.typeMismatch(_, context) {
-            // fatalError("Failed to decode due to type mismatch – \(context.debugDescription)")
-            throw DecodeJSONError.typeMismatch(description: context.debugDescription)
+            throw DecodeJSONError.typeMismatch(
+                error: "Failed to decode due to type mismatch – \(context.debugDescription)" as! Error
+            )
         } catch let DecodingError.valueNotFound(type, context) {
-            // fatalError("Failed to decode due to missing \(type) value – \(context.debugDescription)")
-            throw DecodeJSONError.valueNotFound(type: type, description: context.debugDescription)
+            throw DecodeJSONError.valueNotFound(
+                error: "Failed to decode due to missing \(type) value – \(context.debugDescription)" as! Error
+            )
         } catch DecodingError.dataCorrupted(_) {
-            //fatalError("Failed to decode because it appears to be invalid JSON.")
-            throw DecodeJSONError.dataCorrupted(description: "Failed to decode because it appears to be invalid JSON.")
+            throw DecodeJSONError.dataCorrupted(
+                error: "Failed to decode because it appears to be invalid JSON." as! Error
+            )
         } catch {
-            fatalError("Failed to decode: \(error.localizedDescription)")
+            throw DecodeJSONError.unknownError(
+                error: "Failed to decode: \(error.localizedDescription)" as! Error
+            )
         }
     }
 }
@@ -47,5 +53,41 @@ extension DecodeJSON {
         case valueNotFound(error: Error)
         case dataCorrupted(error: Error)
         case unknownError(error: Error)
+    }
+}
+
+extension DecodeJSON.DecodeJSONError {
+    var errorDescription: String? {
+        switch self {
+            case let .keyNotFound(error):
+                return error.localizedDescription
+            case let .typeMismatch(error):
+                return error.localizedDescription
+            case let .valueNotFound(error):
+                return error.localizedDescription
+            case let .dataCorrupted(error):
+                return error.localizedDescription
+            case let .unknownError(error):
+                return error.localizedDescription
+        }
+    }
+}
+
+extension DecodeJSON.DecodeJSONError: Equatable {
+    static func == (lhs: DecodeJSON.DecodeJSONError, rhs: DecodeJSON.DecodeJSONError) -> Bool {
+        switch (lhs, rhs) {
+            case (.keyNotFound, .keyNotFound):
+                return true
+            case (.typeMismatch, .typeMismatch):
+                return true
+            case (.valueNotFound, .valueNotFound):
+                return true
+            case (.dataCorrupted, .dataCorrupted):
+                return true
+            case (.unknownError, .unknownError):
+                return true
+            default:
+                return false
+        }
     }
 }
