39d29fa4af5d6c7afc0dcfc9203d57095ce46f22
simontopliss <simontopliss@me.com>
2023-07-15 00:56:09 +0100

    Add connector type graphics

--- EVChargePoints/Features/Charge Point List/ChargePointRow.swift
+++ EVChargePoints/Features/Charge Point List/ChargePointRow.swift
@@ -14,18 +14,43 @@ struct ChargePointRow: View {
     var address: Address {
         vm.createAddress(chargeDevice: chargeDevice)
     }
+    var connectorGraphicsAndCounts: [ConnectorGraphic] {
+        vm.getConnectorGraphicsAndCounts(connectors: chargeDevice.connector)
+    }
 
     var body: some View {
         VStack(alignment: .leading) {
             Text(chargeDevice.chargeDeviceName)
                 .font(.title2)
+                .fontWeight(.semibold)
 
             Text("Ref: \(chargeDevice.chargeDeviceRef)")
                 .font(.caption)
 
+            Text(chargeDevice.attribution)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+
             Text(address.singleLineAddress)
                 .font(.caption)
+
+            HStack {
+                // Get the connector types and add a count next to the graphic
+                ForEach(connectorGraphicsAndCounts) { connector in
+                    HStack {
+                        Image(connector.name)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        .frame(height: 22)
+                        
+                        Text("\(connector.count)")
+                            .font(.footnote)
+                    }
+                    .padding(.trailing, 6)
+                }
+            }
         }
+        Divider()
     }
 }
 
@@ -34,4 +59,5 @@ struct ChargePointRow: View {
         vm: ChargePointViewModel(),
         chargeDevice: ChargePointData.mockChargeDevice
     )
+    .padding()
 }
