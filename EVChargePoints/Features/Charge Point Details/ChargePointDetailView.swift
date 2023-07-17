96577dce288c842fc13beb91f74084ac77cfa695
simontopliss <simontopliss@me.com>
2023-07-17 14:56:42 +0100

    Create form with sections for ChargePointDetailView

--- EVChargePoints/Features/Charge Point Details/ChargePointDetailView.swift
+++ EVChargePoints/Features/Charge Point Details/ChargePointDetailView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ChargePointDetailView: View {
 
-    var vm: ChargePointViewModel
-    var chargeDevice: ChargeDevice
-    
+    let vm: ChargePointViewModel
+    let chargeDevice: ChargeDevice
+    var address: Address {
+        vm.createAddress(chargeDevice: chargeDevice)
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            LocationSection(chargeDevice: chargeDevice, address: address)
+            ParkingSection(chargeDevice: chargeDevice)
+            PaymentSection(chargeDevice: chargeDevice)
+        }
+        .navigationTitle(chargeDevice.chargeDeviceName)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
@@ -24,3 +33,118 @@ struct ChargePointDetailView: View {
     )
     .environmentObject(ChargePointViewModel())
 }
+
+struct LocationSection: View {
+    let chargeDevice: ChargeDevice
+    let address: Address
+
+    var body: some View {
+        Section("LOCATION") {
+            LabeledContent {
+                FormText(text: chargeDevice.locationType.rawValue)
+            } label: {
+                FormLabel(label: "TYPE")
+            }
+
+            LabeledContent {
+                FormText(text: address.fullAddress)
+            } label: {
+                FormLabel(label: "ADDRESS")
+            }
+        }
+    }
+}
+
+struct ParkingSection: View {
+    let chargeDevice: ChargeDevice
+
+    var validSection: Bool {
+        Validator.isValid(chargeDevice.parkingFeesDetails) || Validator.isValid(chargeDevice.accessRestrictionDetails) || Validator.isValid(chargeDevice.physicalRestrictionText) ? true : false
+    }
+
+    var body: some View {
+        if validSection {
+            Section("PARKING") {
+                if Validator.isValid(chargeDevice.parkingFeesDetails) {
+                    LabeledContent {
+                        FormText(text: chargeDevice.parkingFeesDetails!)
+                    } label: {
+                        FormLabel(label: "DETAILS")
+                    }
+                }
+                
+                if Validator.isValid(chargeDevice.accessRestrictionDetails) {
+                    LabeledContent {
+                        FormText(text: chargeDevice.accessRestrictionDetails!)
+                    } label: {
+                        FormLabel(label: "ACCESS")
+                    }
+                }
+                
+                if Validator.isValid(chargeDevice.physicalRestrictionText) {
+                    LabeledContent {
+                        FormText(text: chargeDevice.physicalRestrictionText!)
+                    } label: {
+                        FormLabel(label: "RESTRICTIONS")
+                    }
+                }
+            }
+        } else {
+            EmptyView()
+        }
+    }
+}
+
+struct PaymentSection: View {
+    let chargeDevice: ChargeDevice
+
+    var body: some View {
+        Section("PAYMENT") {
+            LabeledContent {
+                FormText(text: chargeDevice.attribution)
+            } label: {
+                FormLabel(label: "NETWORK")
+            }
+
+
+            LabeledContent {
+                FormText(text: chargeDevice.paymentRequiredFlag ? Symbols.no : Symbols.yes)
+            } label: {
+                FormLabel(label: "FREE TO USE")
+            }
+            
+            if Validator.isValid(chargeDevice.paymentDetails) {
+                LabeledContent {
+                    FormText(text: chargeDevice.paymentDetails!)
+                } label: {
+                    FormLabel(label: "DETAILS")
+                }
+            }
+        }
+    }
+}
+
+struct FormText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+            .font(.subheadline)
+            .multilineTextAlignment(.leading)
+            .foregroundColor(Colors.textColor)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct FormLabel: View {
+    let label: String
+
+    var body: some View {
+        Text(label)
+            .font(.subheadline)
+            .multilineTextAlignment(.leading)
+            .frame(width: 80, alignment: .leading)
+            .foregroundColor(.secondary)
+    }
+}
+
