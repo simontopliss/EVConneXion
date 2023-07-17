fd5b1814a658e10c85314c5a81fc04f4918c15f3
simontopliss <simontopliss@me.com>
2023-07-17 12:06:07 +0100

    Create new Symbols constants file for SF Symbol names

---
+++ EVChargePoints/Resources/Constants/Symbols.swift
@@ -0,0 +1,12 @@
+//
+//  Symbols.swift
+//  EVChargePoints
+//
+//  Created by Simon Topliss on 17/07/2023.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let genericNetwork = Image(systemName: "network")
+}
