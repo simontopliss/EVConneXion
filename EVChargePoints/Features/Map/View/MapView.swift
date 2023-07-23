288918b4db5d9d73fb953757fc3b05ec009fafc5
simontopliss <simontopliss@me.com>
2023-07-23 16:53:09 +0100

    Update MapView to use accent color

--- EVChargePoints/Features/Map/View/MapView.swift
+++ EVChargePoints/Features/Map/View/MapView.swift
@@ -15,12 +15,12 @@ struct MapView: View {
 
     @StateObject private var vm = MapViewModel()
 
+    // TODO: Ask for permissions to get the user's location and store in UserDefaults
     @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
     @State private var mapSelection: MKMapItem?
-    @State private var cameraHeight: CLLocationDistance = 5000 // Distance in metres
+//    @State private var cameraHeight: CLLocationDistance = 5000 // Distance in metres
 
     var body: some View {
-
         Map(position: $cameraPosition, selection: $mapSelection) {
 
             Annotation("My location", coordinate: .userLocation) {
@@ -32,7 +32,7 @@ struct MapView: View {
                     Circle()
                         .frame(width: 20, height: 20)
                         .foregroundColor(.white)
-                    
+
                     Circle()
                         .frame(width: 12, height: 12)
                         .foregroundColor(.pink)
@@ -42,13 +42,13 @@ struct MapView: View {
             ForEach(chargePointViewModel.chargeDevices) { chargeDevice in
                 if let chargeDeviceCoordinate = vm.coordinateFor(chargeDevice.chargeDeviceLocation) {
                     let markerName = chargePointViewModel.displayNameFor(network: chargeDevice.attribution)
-                    
+
                     Marker(
                         markerName,
                         systemImage: Symbols.evChargerName,
                         coordinate: chargeDeviceCoordinate
                     )
-                    .tint(.green)
+                    .tint(Color.accentColor)
                 }
             }
         }
@@ -65,4 +65,3 @@ struct MapView: View {
         .environmentObject(ChargePointViewModel())
         .environmentObject(NavigationRouter())
 }
-
