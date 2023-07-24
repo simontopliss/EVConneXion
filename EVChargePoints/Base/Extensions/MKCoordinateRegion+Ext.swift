fde658e1a612912ab027e96836d53dcff30b586c
simontopliss <simontopliss@me.com>
2023-07-24 18:41:39 +0100

    Change defaultRegion to London Eye coordinates

--- EVChargePoints/Base/Extensions/MKCoordinateRegion+Ext.swift
+++ EVChargePoints/Base/Extensions/MKCoordinateRegion+Ext.swift
@@ -5,7 +5,7 @@ extension MKCoordinateRegion {
 
     static var defaultRegion: MKCoordinateRegion {
         .init(
-            center: CLLocationCoordinate2D(latitude: 37.33233141, longitude: -122.03121860),
+            center: .defaultLocation,
             span: MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
         )
     }
