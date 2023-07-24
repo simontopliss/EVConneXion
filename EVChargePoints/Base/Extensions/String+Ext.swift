b29454cf64947d97938b6b5589730a4955ced6c0
simontopliss <simontopliss@me.com>
2023-07-24 18:41:56 +0100

    Add capitalizingFirstLetter String extension

--- EVChargePoints/Base/Extensions/String+Ext.swift
+++ EVChargePoints/Base/Extensions/String+Ext.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 extension String {
+
     func trim() -> String {
         self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = capitalizingFirstLetter()
     }
 }
