942fdade9401bb3e83716e66695926f6d723a319
simontopliss <simontopliss@me.com>
2023-07-25 20:26:39 +0100

    Create extension for LocationManager for the defaultLocation and defaultRegion

--- EVChargePoints/Base/Managers/LocationManager.swift
+++ EVChargePoints/Base/Managers/LocationManager.swift
@@ -11,8 +11,9 @@ import MapKit
 
 class LocationManager: NSObject, ObservableObject {
 
-    @Published var region: MKCoordinateRegion = .defaultRegion
+    @Published var region: MKCoordinateRegion = LocationManager.defaultRegion
     let locationManager = CLLocationManager()
+//    var route: MKRoute!
 
     override init() {
         super.init()
@@ -24,9 +25,24 @@ class LocationManager: NSObject, ObservableObject {
     }
 }
 
+extension LocationManager {
+
+    static var defaultLocation: CLLocationCoordinate2D {
+        return .init(latitude: 51.503351, longitude: -0.119623)
+    }
+
+    static var defaultRegion: MKCoordinateRegion {
+        .init(
+            center: self.defaultLocation,
+            span: MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
+        )
+    }
+}
+
 extension LocationManager: CLLocationManagerDelegate {
 
     private func checkAuthorization() {
+        print(#function)
 
         switch locationManager.authorizationStatus {
             case .notDetermined:
@@ -48,11 +64,13 @@ extension LocationManager: CLLocationManagerDelegate {
     }
 
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
         checkAuthorization()
     }
 
     // Required delegate conformance
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function)
 
         guard let location = locations.last else { return }
 
@@ -68,4 +86,21 @@ extension LocationManager: CLLocationManagerDelegate {
         print(String(describing: error))
     }
 
+}
+
+extension LocationManager {
+
+    static func getDistanceToDestination(srcMapItem: MKMapItem, destMapItem: MKMapItem) -> MKRoute? {
+        let request = MKDirections.Request() //create a direction request object
+        request.source = srcMapItem //this is the source location mapItem object
+        request.destination = destMapItem //this is the destination location mapItem object
+        request.transportType = MKDirectionsTransportType.automobile //define the transportation method
+
+        Task {
+            let result = try? await MKDirections(request: request).calculate()
+            return result?.routes.first
+        }
+
+        return nil
+    }
 }
