c077a2d8ede9b63e82fdea7a4d31ea79ec9ba8ff
simontopliss <simontopliss@me.com>
2023-07-27 19:58:32 +0100

    Add deviceMapMarker to ChargeDevice

--- EVChargePoints/Base/Model/National Chargepoint Registry/ChargeDevice.swift
+++ EVChargePoints/Base/Model/National Chargepoint Registry/ChargeDevice.swift
@@ -18,6 +18,7 @@ struct ChargeDevice: Decodable, Identifiable {
     var chargeDeviceName: String
     var chargeDeviceText: String?
     var chargeDeviceLocation: ChargeDeviceLocation
+    var deviceMapMarker: DeviceMapMarker // Map Item
     var chargeDeviceModel: String?
     var publishStatusId: String
     var dateCreated: String
@@ -89,6 +90,54 @@ struct ChargeDevice: Decodable, Identifiable {
         case locationType               = "LocationType"
         case bearing                    = "Bearing"
         case accessible24Hours          = "Accessible24Hours"
+    }
+}
+
+extension ChargeDevice {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.chargeDeviceID = try container.decode(String.self, forKey: .chargeDeviceID)
+        self.chargeDeviceRef = try container.decode(String.self, forKey: .chargeDeviceRef)
+        self.chargeDeviceName = try container.decode(String.self, forKey: .chargeDeviceName)
+        self.chargeDeviceText = try container.decodeIfPresent(String.self, forKey: .chargeDeviceText)
+        self.chargeDeviceLocation = try container.decode(ChargeDeviceLocation.self, forKey: .chargeDeviceLocation)
+        self.chargeDeviceModel = try container.decodeIfPresent(String.self, forKey: .chargeDeviceModel)
+        self.publishStatusId = try container.decode(String.self, forKey: .publishStatusId)
+        self.dateCreated = try container.decode(String.self, forKey: .dateCreated)
+        self.dateUpdated = try container.decode(String.self, forKey: .dateUpdated)
+        self.attribution = try container.decode(String.self, forKey: .attribution)
+        self.dateDeleted = try container.decode(String.self, forKey: .dateDeleted)
+        self.connector = try container.decode([Connector].self, forKey: .connector)
+        self.deviceOwner = try container.decode(DeviceOwner.self, forKey: .deviceOwner)
+        self.deviceController = try container.decode(DeviceController.self, forKey: .deviceController)
+        self.deviceAccess = try container.decodeIfPresent(DeviceAccess.self, forKey: .deviceAccess)
+        self.deviceNetworks = try container.decode(String.self, forKey: .deviceNetworks)
+        self.chargeDeviceStatus = try container.decode(ChargeStatus.self, forKey: .chargeDeviceStatus)
+        self.publishStatus = try container.decode(String.self, forKey: .publishStatus)
+        self.deviceValidated = try container.decode(String.self, forKey: .deviceValidated)
+        self.recordModerated = try container.decode(RecordModerated.self, forKey: .recordModerated)
+        self.recordLastUpdated = try container.decodeIfPresent(String.self, forKey: .recordLastUpdated)
+        self.recordLastUpdatedBy = try container.decodeIfPresent(String.self, forKey: .recordLastUpdatedBy)
+        self.paymentRequiredFlag = try container.decode(Bool.self, forKey: .paymentRequiredFlag)
+        self.paymentDetails = try container.decodeIfPresent(String.self, forKey: .paymentDetails)
+        self.subscriptionRequiredFlag = try container.decode(Bool.self, forKey: .subscriptionRequiredFlag)
+        self.subscriptionDetails = try container.decodeIfPresent(String.self, forKey: .subscriptionDetails)
+        self.parkingFeesFlag = try container.decode(Bool.self, forKey: .parkingFeesFlag)
+        self.parkingFeesDetails = try container.decodeIfPresent(String.self, forKey: .parkingFeesDetails)
+        self.parkingFeesUrl = try container.decodeIfPresent(String.self, forKey: .parkingFeesUrl)
+        self.accessRestrictionFlag = try container.decode(Bool.self, forKey: .accessRestrictionFlag)
+        self.accessRestrictionDetails = try container.decodeIfPresent(String.self, forKey: .accessRestrictionDetails)
+        self.physicalRestrictionFlag = try container.decode(Bool.self, forKey: .physicalRestrictionFlag)
+        self.physicalRestrictionText = try container.decodeIfPresent(String.self, forKey: .physicalRestrictionText)
+        self.onStreetFlag = try container.decode(Bool.self, forKey: .onStreetFlag)
+        self.locationType = try container.decode(LocationType.self, forKey: .locationType)
+        self.bearing = try container.decodeIfPresent(String.self, forKey: .bearing)
+        self.accessible24Hours = try container.decode(Bool.self, forKey: .accessible24Hours)
+
+        self.deviceMapMarker = DeviceMapMarker(
+            latitude: Double(chargeDeviceLocation.latitude) ?? 0.0,
+            longitude: Double(chargeDeviceLocation.longitude) ?? 0.0
+        )
     }
 }
 
