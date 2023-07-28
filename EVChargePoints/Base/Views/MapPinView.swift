f6374db669cb625dacfe22cff90a7000a1f3b967
simontopliss <simontopliss@me.com>
2023-07-28 22:12:03 +0100

    Increase size of bolt

--- EVChargePoints/Base/Views/MapPinView.swift
+++ EVChargePoints/Base/Views/MapPinView.swift
@@ -20,20 +20,16 @@ struct MapPinView: View {
         MapPin()
             .fill(pinColor)
             .frame(width: width, height: height, alignment: .center)
-//            .overlay {
-//                MapPin()
-//                    .stroke(Color.black, lineWidth: 1.0)
-//            }
             .shadow(color: .secondary, radius: 3.0)
             .overlay {
                 Image(systemName: "bolt.fill")
                     .foregroundColor(.white)
-                    .scaleEffect(0.85)
+                    .scaleEffect(0.90)
                     .offset(y: -1.75)
             }
     }
 }
 
 #Preview {
-    MapPinView(height: 30.0, pinColor: .cyan)
+    MapPinView(height: 30.0, pinColor: .accentColor)
 }
