b9afbf1e3526b91674f76834034d358f726dc807
simontopliss <simontopliss@me.com>
2023-07-28 19:31:54 +0100

    Implement presentationDetents, overlay and sheet in MapView

--- EVChargePoints/Features/Map/MapView.swift
+++ EVChargePoints/Features/Map/MapView.swift
@@ -16,17 +16,23 @@ struct MapView: View {
 
     @StateObject private var mapViewModel = MapViewModel()
 
-    // Map Properties
-    let span = MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
+    /// Map Properties
+    //let span = MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
     @State private var cameraPosition: MapCameraPosition = .region(LocationManager.defaultRegion)
-    @State private var mapSelection: MKMapItem?
     @State private var deviceSelection: ChargeDevice?
+    @State private var viewingRegion: MKCoordinateRegion?
     @Namespace private var locationSpace
 
+    /// Map Selection Detail Properties
     @State private var showDetails = false
-    @State private var getDirections = false
+    @State private var lookAroundScene: MKLookAroundScene?
 
-    // User Location Animation
+    /// Route Properties
+    @State private var routeDisplaying: Bool = false
+    @State private var route: MKRoute?
+    @State private var routeDestination: ChargeDevice?
+
+    /// User Location Animation
     @State private var delay: Double = 0
     @State private var scale: CGFloat = 0.5
     @State private var duration = 0.8
@@ -45,39 +51,185 @@ struct MapView: View {
 
                 ForEach(chargePointViewModel.chargeDevices) { chargeDevice in
 
-                    Annotation(chargeDevice.chargeDeviceName, coordinate: chargeDevice.deviceMapMarker.coordinate) {
+                    Annotation(chargeDevice.chargeDeviceName, coordinate: chargeDevice.deviceMapItem.coordinate) {
                         Button {
-                            let _ = print(chargeDevice.deviceMapMarker.distanceFromUser)
+                            deviceSelection = chargeDevice
+                            withAnimation(.snappy) {
+                                // TODO: Move the camera up a bit to accommodate the MapDetails detent
+                                cameraPosition = .region(chargeDevice.deviceMapItem.region)
+                            }
                         } label: {
                             MapPinView(pinColor: networkColor(attribution: chargeDevice.attribution))
                         }
                     }
                     .tag(chargeDevice.id)
                 }
-            }
-            .sheet(isPresented: $showDetails) {
-                if let deviceSelection {
-                    LocationDetailsView(
-                        mapSelection: $mapSelection,
-                        chargeDevice: deviceSelection,
-                        show: $showDetails,
-                        getDirections: $getDirections
-                    )
+
+                /// Display Route using Polyline
+                if let route {
+                    MapPolyline(route.polyline)
+                        /// Applying Bigger Stroke
+                        .stroke(.blue, lineWidth: 7)
                 }
             }
-            .presentationDetents([.height(340)])
-            .presentationBackgroundInteraction(.enabled(upThrough: .height(340)))
-            .presentationCornerRadius(12)
-            .mapControls {
-                MapCompass()
-                MapPitchToggle()
-                MapUserLocationButton()
+            .onMapCameraChange { context in
+                viewingRegion = context.region
+            }
+            .overlay(alignment: .bottomTrailing) {
+                VStack(spacing: 15) {
+                    MapCompass(scope: locationSpace)
+                    MapPitchToggle(scope: locationSpace)
+                    /// As this will work only when the User Gave Location Access
+                    MapUserLocationButton(scope: locationSpace)
+                    /// This will Goes to the Defined User Region
+                    Button {
+                        withAnimation(.smooth) {
+                            cameraPosition = .region(LocationManager.defaultRegion)
+                        }
+                    } label: {
+                        Image(systemName: "mappin")
+                            .font(.title3)
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                .buttonBorderShape(.circle)
+                .padding()
             }
             .mapScope(locationSpace)
             .navigationTitle("Map")
             .navigationBarTitleDisplayMode(.inline)
             .toolbarBackground(.visible, for: .navigationBar)
             .toolbarBackground(.ultraThinMaterial, for: .navigationBar)
+            .sheet(isPresented: $showDetails, onDismiss: {
+                withAnimation(.snappy) {
+                    /// Zooming Region
+                    if let boundingRect = route?.polyline.boundingMapRect, routeDisplaying {
+                        cameraPosition = .rect(boundingRect.reducedRect(0.45))
+                    }
+                }
+            }, content: {
+                mapDetails()
+                    .presentationDetents([.height(300)])
+                    .presentationBackgroundInteraction(.enabled(upThrough: .height(300)))
+                    .presentationCornerRadius(25)
+                    .interactiveDismissDisabled(true)
+            })
+            .safeAreaInset(edge: .bottom) {
+                if routeDisplaying {
+                    Button("End Route") {
+                        /// Closing The Route and Setting the Selection
+                        withAnimation(.snappy) {
+                            routeDisplaying = false
+                            showDetails = true
+                            deviceSelection = routeDestination
+                            routeDestination = nil
+                            route = nil
+                            if let coordinate = deviceSelection?.deviceMapItem.coordinate {
+                                cameraPosition = .region(
+                                    .init(
+                                        center: coordinate,
+                                        latitudinalMeters: 5000,
+                                        longitudinalMeters: 5000
+                                    )
+                                )
+                            }
+                        }
+                    }
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .contentShape(Rectangle())
+                    .padding(.vertical, 12)
+                    .background(.red.gradient, in: .rect(cornerRadius: 15))
+                    .padding()
+                    .background(.ultraThinMaterial)
+                }
+            }
+        }
+        .onChange(of: deviceSelection) { _, newValue in
+            /// Displaying Details about the Selected Place
+            showDetails = newValue != nil
+            /// Fetching Look Around Preview, when ever selection Changes
+            fetchLookAroundPreview()
+        }
+    }
+
+    /// Map Details View
+    @ViewBuilder
+    func mapDetails() -> some View {
+        VStack(spacing: 15) {
+            ZStack {
+                /// New Look Around API
+                if lookAroundScene == nil {
+                    /// New Empty View API
+                    ContentUnavailableView("No Preview Available", systemImage: "eye.slash")
+                } else {
+                    LookAroundPreview(scene: $lookAroundScene)
+                }
+            }
+            .frame(height: 200)
+            .clipShape(.rect(cornerRadius: 15))
+            /// Close Button
+            .overlay(alignment: .topTrailing) {
+                Button(action: {
+                    /// Closing View
+                    showDetails = false
+                    withAnimation(.snappy) {
+                        deviceSelection = nil
+                    }
+                }, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(.black)
+                        .background(.white, in: .circle)
+                })
+                .padding(10)
+            }
+
+            /// Direction's Button
+            Button("Get Directions", action: fetchRoute)
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 12)
+                .contentShape(Rectangle())
+                .background(.blue.gradient, in: .rect(cornerRadius: 15))
+        }
+        .padding(15)
+    }
+
+    /// Fetching Location Preview
+    func fetchLookAroundPreview() {
+        if let deviceSelection {
+            /// Clearing Old One
+            lookAroundScene = nil
+            Task {
+                let request = MKLookAroundSceneRequest(
+                    coordinate: deviceSelection.deviceMapItem.coordinate
+                )
+                lookAroundScene = try? await request.scene
+            }
+        }
+    }
+
+    /// Fetching Route
+    func fetchRoute() {
+        if let deviceSelection {
+            let request = MKDirections.Request()
+            request.source = .init(
+                placemark: .init(coordinate: locationManager.userLocation)
+            )
+            request.destination = deviceSelection.deviceMapItem.mapItem
+
+            Task {
+                let result = try? await MKDirections(request: request).calculate()
+                route = result?.routes.first
+                /// Saving Route Destination
+                routeDestination = deviceSelection
+
+                withAnimation(.snappy) {
+                    routeDisplaying = true
+                    showDetails = false
+                }
+            }
         }
     }
 }
@@ -125,5 +277,22 @@ extension MapView {
 
     func networkColor(attribution: String) -> Color {
         chargePointViewModel.networkColorFor(network: attribution) ?? Color.accentColor
+    }
+}
+
+extension MKMapRect {
+    func reducedRect(_ fraction: CGFloat = 0.35) -> MKMapRect {
+        var regionRect = self
+
+        let wPadding = regionRect.size.width * fraction
+        let hPadding = regionRect.size.height * fraction
+
+        regionRect.size.width += wPadding
+        regionRect.size.height += hPadding
+
+        regionRect.origin.x -= wPadding / 2
+        regionRect.origin.y -= hPadding / 2
+
+        return regionRect
     }
 }
