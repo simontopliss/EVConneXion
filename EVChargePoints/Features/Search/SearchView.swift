4ed2e459040c4764cb93ac6f7b0e0e72de921ea4
simontopliss <simontopliss@me.com>
2023-08-03 21:23:52 +0100

    Update SearchView

--- EVChargePoints/Features/Search/SearchView.swift
+++ EVChargePoints/Features/Search/SearchView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct SearchView: View {
+
+    @Binding var showSheet: Bool
+
+    // TDOD: Store in UserDefautlts
+    let recentSearches = ["Bingley, BD16 1BE, England", "Beverley, HU17 ORX, England", "Erewash, DE7 8LN, England"]
+
     var body: some View {
-        Text("Hello, SearchView!")
+        VStack {
+            // TODO: Show recent searches
+            List { // Add selection
+                Section("Recent Searches") {
+                    ForEach(recentSearches, id: \.self) {
+                        Text($0)
+                            .foregroundStyle(AppColors.textColor)
+                    }
+                }
+            }
+            .listStyle(InsetListStyle())
+        }
+        .padding(.top)
+        .overlay(alignment: .topTrailing) {
+            Button(role: .cancel) {
+                showSheet.toggle()
+            } label: {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.title)
+                    .foregroundStyle(.black.opacity(0.20))
+                    .background(.white, in: .circle)
+            }
+            .padding([.top, .trailing])
+        }
     }
 }
 
 #Preview {
-    SearchView()
+    SearchView(showSheet: .constant(true))
 }
