7c617a902e7d828997e3f28d5a859f5eb5b093fe
simontopliss <simontopliss@me.com>
2023-08-04 19:57:57 +0100

    Refactor FiltersViewModel to add Filter struct

--- EVChargePoints/Features/Filters/FiltersViewModel.swift
+++ EVChargePoints/Features/Filters/FiltersViewModel.swift
@@ -8,18 +8,59 @@
 import Foundation
 import SwiftUI
 
+struct Filter: Identifiable {
+    var id = UUID()
+    var title: String
+    var symbol: Image
+    var destination: Route
+}
+
 final class FiltersViewModel: ObservableObject {
 
     @Published var accessData: [AccessData] = []
     @Published var chargerData: ChargerData = ChargerData()
     @Published var locationData: [LocationData] = []
     @Published var paymentData: [PaymentData] = []
+    @Published var filters: [Filter] = []
 
     init() {
         /// Load JSON files
         loadAccessData()
         loadLocationData()
         loadPaymentData()
+
+        filters = [
+            Filter(
+                title: "Access",
+                symbol: Symbols.accessSymbol,
+                destination: Route.filterAccessTypesView
+            ),
+            Filter(
+                title: "Charger",
+                symbol: Symbols.chargerSymbol,
+                destination: Route.filterChargerTypesView
+            ),
+            Filter(
+                title: "Connector",
+                symbol: Symbols.connectorSymbol,
+                destination: Route.filterConnectorTypesView
+            ),
+            Filter(
+                title: "Location",
+                symbol: Symbols.locationSymbol,
+                destination: Route.filterLocationTypesView
+            ),
+            Filter(
+                title: "Network",
+                symbol: Symbols.networkSymbol,
+                destination: Route.filterNetworkTypesView
+            ),
+            Filter(
+                title: "Payment",
+                symbol: Symbols.paymentSymbol,
+                destination: Route.filterPaymentTypesView
+            )
+        ]
     }
 
     func loadAccessData() {
@@ -43,3 +84,14 @@ final class FiltersViewModel: ObservableObject {
         )
     }
 }
+
+//extension FiltersViewModel {
+//    enum Filters: String, CaseIterable {
+//        case access      = "Access"
+//        case connectors  = "Connectors"
+//        case locations   = "Locations"
+//        case networks    = "Networks"
+//        case payment     = "Payment"
+//        case chargers    = "Chargers"
+//    }
+//}
