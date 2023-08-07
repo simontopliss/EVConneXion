4693f92cefa0c0fe71a4f0350d6c73c774fbdff7
simontopliss <simontopliss@me.com>
2023-08-07 19:24:57 +0100

    Create new fire for XmarkButtonView

---
+++ EVChargePoints/Base/Views/XmarkButtonView.swift
@@ -0,0 +1,26 @@
+//
+//  XmarkButtonView.swift
+//  EVChargePoints
+//
+//  Created by Simon Topliss on 28/07/2023.
+//
+
+import SwiftUI
+
+struct XmarkButtonView: View {
+
+    var foregroundColor: Color = .primary
+    var backgroundColor: Color = Color(.tertiarySystemFill)
+
+    var body: some View {
+        Image(systemName: "xmark.circle.fill")
+            .font(.title)
+            .foregroundStyle(foregroundColor)
+            .background(backgroundColor, in: .circle)
+    }
+}
+
+#Preview {
+    XmarkButtonView()
+        .environment(\.colorScheme, .dark)
+}
