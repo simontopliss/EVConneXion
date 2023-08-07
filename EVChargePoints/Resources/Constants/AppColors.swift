dcffcf2c13c80d3dba3454016499b75134972aa9
simontopliss <simontopliss@me.com>
2023-08-07 19:28:16 +0100

    Add new static constants for light and dark green

--- EVChargePoints/Resources/Constants/AppColors.swift
+++ EVChargePoints/Resources/Constants/AppColors.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 enum AppColors {
+    static let lightGreen       = Color("LightGreen")
+    static let darkGreen        = Color("DarkGreen")
     static let textColor        = Color("TextColor")
     static let backgroundColor  = Color("BackgroundColor")
     static let listRowColor     = Color("ListRowColor")
