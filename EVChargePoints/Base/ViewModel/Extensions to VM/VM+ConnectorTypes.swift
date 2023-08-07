4090b9bc480ba2435107966525f9d2eb17177a1a
simontopliss <simontopliss@me.com>
2023-08-07 19:22:39 +0100

    Add connectorGraphicFor and displayNameFor to VM+ConnectorTypes

--- EVChargePoints/Base/ViewModel/Extensions to VM/VM+ConnectorTypes.swift
+++ EVChargePoints/Base/ViewModel/Extensions to VM/VM+ConnectorTypes.swift
@@ -16,4 +16,26 @@ extension ChargePointViewModel {
             type: [ConnectorData].self
         )
     }
+
+    func connectorGraphicFor(connectorType: String, colorScheme: ColorScheme) -> String {
+        let item = connectorData.first { $0.dataName == connectorType }
+
+        guard let graphicName = item?.graphicName else {
+            // TODO: Should probably handle a missing graphic
+            print("No connector graphic found for \(connectorType)")
+            return "default-network-128"
+        }
+
+        return colorScheme == .dark ? graphicName + "-i" : graphicName
+    }
+
+    func displayNameFor(connectorType: String) -> String {
+        let item = connectorData.first { $0.dataName == connectorType }
+
+        guard let displayName = item?.displayName else {
+            fatalError("No display name for for \(connectorType)")
+        }
+
+        return displayName
+    }
 }
