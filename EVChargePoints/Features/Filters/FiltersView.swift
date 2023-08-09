4a7f3a2b70b3479b55d44eb0defeac3d514a5d4c
simontopliss <simontopliss@me.com>
2023-08-09 12:00:37 +0100

    Delete FiltersViewModel.swift and move contents to FiltersView

--- EVChargePoints/Features/Filters/FiltersView.swift
+++ EVChargePoints/Features/Filters/FiltersView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct FiltersView: View {
 
-    @EnvironmentObject private var filtersViewModel: FiltersViewModel
+    @StateObject private var filtersViewModel = FiltersViewModel()
     @EnvironmentObject private var routerManager: NavigationRouter
     @EnvironmentObject private var dataManager: DataManager
 
@@ -150,3 +150,57 @@ struct FilterNavigationLink: View {
         }
     }
 }
+
+final class FiltersViewModel: ObservableObject {
+
+    @Published var filters: [Filter] = []
+
+    init() {
+        createFilters()
+    }
+
+    func createFilters() {
+        filters = [
+            Filter(
+                title: "Access",
+                symbol: Symbols.accessSymbol,
+                destination: Route.filterAccessTypesView
+            ),
+            Filter(
+                title: "Charger",
+                symbol: Symbols.chargerSymbol,
+                destination: Route.filterChargerTypesView
+            ),
+            Filter(
+                title: "Connector",
+                symbol: Symbols.connectorSymbol,
+                destination: Route.filterConnectorTypesView
+            ),
+            Filter(
+                title: "Location",
+                symbol: Symbols.locationSymbol,
+                destination: Route.filterLocationTypesView
+            ),
+            Filter(
+                title: "Network",
+                symbol: Symbols.networkSymbol,
+                destination: Route.filterNetworkTypesView
+            ),
+            Filter(
+                title: "Payment",
+                symbol: Symbols.paymentSymbol,
+                destination: Route.filterPaymentTypesView
+            )
+        ]
+    }
+}
+
+extension FiltersViewModel {
+    struct Filter: Identifiable {
+        var id = UUID()
+        var title: String
+        var symbol: Image
+        var destination: Route
+    }
+}
+
