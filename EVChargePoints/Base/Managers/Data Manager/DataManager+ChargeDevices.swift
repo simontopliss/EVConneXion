3778019a910ffd13e48bea5a9f3f1b2b167e7cfb
simontopliss <simontopliss@me.com>
2023-08-10 23:22:24 +0100

    Fix bug with the property chargeDevices being used instead of the local version

--- EVChargePoints/Base/Managers/Data Manager/DataManager+ChargeDevices.swift
+++ EVChargePoints/Base/Managers/Data Manager/DataManager+ChargeDevices.swift
@@ -9,25 +9,32 @@ import Foundation
 
 extension DataManager {
 
-    struct NameAndPostcode: Hashable {
+    struct DeviceNameAndPostcode: Hashable {
         let chargeDeviceName: String
         let postcode: String
     }
 
-    func sortAndRemoveDuplicateDevices(chargeDevices: [ChargeDevice]) -> [ChargeDevice] {
-        let deviceNamesAndPostcodes = getDeviceNamesAndPostcodes(chargeDevices: chargeDevices)
+    func sortAndRemoveDuplicateDevices(devices: [ChargeDevice]) -> [ChargeDevice] {
+        let deviceNamesAndPostcodes = getDeviceNamesAndPostcodes(devices: devices)
         let duplicates = deviceNamesAndPostcodes.duplicates()
-        let duplicateChargeDevices = combineConnectorsFromDuplicateDevices(duplicates: duplicates)
-        let newChargeDevices = removeDuplicatesFromChargeDevices(duplicates: duplicates, chargeDevices: chargeDevices)
+        // dump(duplicates)
+        let duplicateChargeDevices = combineConnectorsFromDuplicateDevices(
+            duplicates: duplicates,
+            devices: devices
+        )
+        let newChargeDevices = removeDuplicatesFromChargeDevices(
+            duplicates: duplicates,
+            devices: devices
+        )
         let finalChargeDevices = (newChargeDevices + duplicateChargeDevices).sorted(
             by: { $0.deviceMapItem.distanceFromUser < $1.deviceMapItem.distanceFromUser }
         )
         return finalChargeDevices
     }
 
-    func getDeviceNamesAndPostcodes(chargeDevices: [ChargeDevice]) -> [NameAndPostcode] {
-        let namesAndPostcodes = chargeDevices.compactMap {
-            NameAndPostcode(
+    func getDeviceNamesAndPostcodes(devices: [ChargeDevice]) -> [DeviceNameAndPostcode] {
+        let namesAndPostcodes = devices.compactMap {
+            DeviceNameAndPostcode(
                 chargeDeviceName: $0.chargeDeviceName,
                 postcode: $0.chargeDeviceLocation.address.postcode
             )
@@ -36,28 +43,40 @@ extension DataManager {
         return namesAndPostcodes
     }
 
-    func combineConnectorsFromDuplicateDevices(duplicates: [NameAndPostcode]) -> [ChargeDevice] {
+    func combineConnectorsFromDuplicateDevices(
+        duplicates: [DeviceNameAndPostcode],
+        devices: [ChargeDevice]
+    ) -> [ChargeDevice] {
         var duplicateChargeDevices: [ChargeDevice] = []
         for duplicate in duplicates {
             let chargeDeviceName = duplicate.chargeDeviceName
             let postcode = duplicate.postcode
-            let filteredChargeDevices = chargeDevices.filter {
+            let filteredChargeDevices = devices.filter {
                 $0.chargeDeviceName == chargeDeviceName &&
                 $0.chargeDeviceLocation.address.postcode == postcode
             }
             let connectors = filteredChargeDevices.flatMap { $0.connector }
-            var firstDevice = filteredChargeDevices.first!
-            firstDevice.connector = connectors
-            duplicateChargeDevices.append(firstDevice)
+            var firstDevice = filteredChargeDevices.first
+            if firstDevice != nil {
+                firstDevice!.connector = connectors
+                duplicateChargeDevices.append(firstDevice!)
+            } else {
+                print(chargeDeviceName)
+                print(postcode)
+                fatalError("Missing duplicates. This shouldn't happen.")
+            }
         }
 
         return duplicateChargeDevices
     }
 
-    func removeDuplicatesFromChargeDevices(duplicates: [NameAndPostcode], chargeDevices: [ChargeDevice]) -> [ChargeDevice] {
+    func removeDuplicatesFromChargeDevices(
+        duplicates: [DeviceNameAndPostcode],
+        devices: [ChargeDevice]
+    ) -> [ChargeDevice] {
         var newChargeDevices: [ChargeDevice] = []
-        for chargeDevice in chargeDevices {
-            let nameAndPostcode = NameAndPostcode(
+        for chargeDevice in devices {
+            let nameAndPostcode = DeviceNameAndPostcode(
                 chargeDeviceName: chargeDevice.chargeDeviceName,
                 postcode: chargeDevice.chargeDeviceLocation.address.postcode
             )
