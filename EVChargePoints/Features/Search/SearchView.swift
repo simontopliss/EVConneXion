b7b68e8df5fd72ec3eec85b5104a7f89a92db78d
simontopliss <simontopliss@me.com>
2023-08-13 23:17:33 +0100

    SearchView WIP

--- EVChargePoints/Features/Search/SearchView.swift
+++ EVChargePoints/Features/Search/SearchView.swift
@@ -16,42 +16,13 @@ struct SearchView: View {
 
     var body: some View {
         VStack {
-            HStack(alignment: .lastTextBaseline) {
-                TextField(
-                    "Search",
-                    text: $dataManager.searchQuery,
-                    prompt: Text("Enter postcode, town or city…")
-                )
-                .focused($isFocused)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .foregroundStyle(AppColors.textColor)
-                .onSubmit {
-                    Task {
-                        await dataManager.searchForChargeDevices()
-                    }
-                }
-                .submitLabel(.search)
 
-                Button(role: .cancel) {
-                    showSheet.toggle()
-                } label: {
-                    XmarkButtonView(foregroundColor: .gray)
-                }
-                .offset(y: 2.5)
-                .padding(.bottom)
-            }
+            searchHeader
 
             Divider()
 
             if dataManager.userSettings.recentSearches.isEmpty {
-
-                ContentUnavailableView(
-                    "No recent searches",
-                    systemImage: Symbols.noRecentSearchesSymbolName,
-                    description: Text("Your search history will automatically be displayed here.")
-                )
-                // .foregroundStyle(AppColors.textColor)
-
+                contentUnavailable
             } else {
 
                 List { // Add selection
@@ -70,6 +41,14 @@ struct SearchView: View {
             Button("OK", role: .cancel) {}
         } message: {
             Text(dataManager.searchErrorMessage)
+        }
+        .alert(isPresented: $dataManager.hasError, error: dataManager.error) {
+            // TODO: Check NetworkManager.NetworkError errorDescription
+            Button("Retry") {
+                Task {
+                    await dataManager.searchForChargeDevices()
+                }
+            }
         }
         .padding(.top)
         .onAppear {
@@ -90,4 +69,44 @@ struct SearchView: View {
     SearchView(showSheet: .constant(true))
         .environmentObject(DataManager())
     // .environment(\.colorScheme, .dark)
+}
+
+extension SearchView {
+
+    var searchHeader: some View {
+        HStack(alignment: .lastTextBaseline) {
+            TextField(
+                "Search",
+                text: $dataManager.searchQuery,
+                prompt: Text("Enter postcode, town or city…")
+            )
+            .focused($isFocused)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .foregroundStyle(AppColors.textColor)
+            .onSubmit {
+                Task {
+                    await dataManager.searchForChargeDevices()
+                }
+                // TODO: Navigate to MapView or ListView if successful and zoom map to region from searchQuery
+            }
+            .submitLabel(.search)
+
+            Button(role: .cancel) {
+                showSheet.toggle()
+            } label: {
+                XmarkButtonView(foregroundColor: .gray)
+            }
+            .offset(y: 2.5)
+            .padding(.bottom)
+        }
+    }
+
+    var contentUnavailable: some View {
+        ContentUnavailableView(
+            "No recent searches",
+            systemImage: Symbols.noRecentSearchesSymbolName,
+            description: Text("Your search history will automatically be displayed here.")
+        )
+        // .foregroundStyle(AppColors.textColor)
+    }
 }
