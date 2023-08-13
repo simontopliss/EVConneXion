b4bf737ed764887753a1ff3557116c348b0adc2d
simontopliss <simontopliss@me.com>
2023-08-13 14:26:34 +0100

    Add Search properties to DataManager

--- EVChargePoints/Base/Managers/Data Manager/DataManager.swift
+++ EVChargePoints/Base/Managers/Data Manager/DataManager.swift
@@ -1,6 +1,7 @@
 import Foundation
 import SwiftUI
 
+@MainActor
 final class DataManager: ObservableObject {
 
     /// Main Data
@@ -14,15 +15,24 @@ final class DataManager: ObservableObject {
     @Published var filteredDevices: [ChargeDevice] = []
     @Published var userSettings: UserSettings!
 
-//    @Published var distance: Double = 10.0
-
+    /// Network Manager
     @Published private(set) var error: NetworkManager.NetworkError?
     @Published private(set) var isLoading = false
     @Published var hasError = false
 
+    /// Filter Settings
     @Published var filtersChanged = false
     @Published var filterResultError = false
     @Published var filterResultErrorMessage = ""
+
+    /// Search
+    @Published var searchError = false
+    @Published var searchErrorMessage = ""
+    @Published var searchQuery: String = "" {
+        didSet {
+            // TODO: filter recentSearches for match
+        }
+    }
 
     // TODO: Is `limit` required?
     private(set) var limit = 0
@@ -49,7 +59,7 @@ final class DataManager: ObservableObject {
 
     // MARK: - API Call
 
-    @MainActor
+    // @MainActor
     func fetchChargeDevices(requestType: Endpoint.RequestType) async {
 
         let url = Endpoint.buildURL(
@@ -77,7 +87,7 @@ final class DataManager: ObservableObject {
         }
     }
 
-    func loadAccessData() {
+    private func loadAccessData() {
         accessData = try! StaticJSONMapper.decode(
             file: "AccessData",
             type: [AccessData].self,
@@ -85,7 +95,7 @@ final class DataManager: ObservableObject {
         )
     }
 
-    func loadChargerData() {
+    private func loadChargerData() {
         chargerData = try! StaticJSONMapper.decode(
             file: "ChargerData",
             type: ChargerData.self,
@@ -93,7 +103,7 @@ final class DataManager: ObservableObject {
         )
     }
 
-    func loadLocationData() {
+    private func loadLocationData() {
         locationData = try! StaticJSONMapper.decode(
             file: "LocationData",
             type: [LocationData].self,
@@ -101,7 +111,7 @@ final class DataManager: ObservableObject {
         )
     }
 
-    func loadPaymentData() {
+    private func loadPaymentData() {
         paymentData = try! StaticJSONMapper.decode(
             file: "PaymentData",
             type: [PaymentData].self,
@@ -109,7 +119,7 @@ final class DataManager: ObservableObject {
         )
     }
 
-    func loadUserSettings() {
+    private func loadUserSettings() {
         userSettings = try! StaticJSONMapper.decode(
             file: "UserSettings",
             type: UserSettings.self
@@ -136,6 +146,7 @@ final class DataManager: ObservableObject {
         filtersChanged = true
     }
 
+    // TODO: Add Network, Location etc
     func anyConnectorSelected() -> Bool {
         return connectorData.first(where: { $0.setting == true }) != nil
     }
