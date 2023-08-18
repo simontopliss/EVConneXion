2a0594c69a76f372cf8ec028b6c782bb49fb8693
simontopliss <simontopliss@me.com>
2023-08-18 21:12:43 +0100

    Update Symbols

--- EVChargePoints/Resources/Constants/Symbols.swift
+++ EVChargePoints/Resources/Constants/Symbols.swift
@@ -9,35 +9,37 @@ import SwiftUI
 
 enum Symbols {
 
-    static let symbolWidth                 = 32.0
-    static let symbolHeight                = 32.0
+    static let symbolWidth                   = 32.0
+    static let symbolHeight                  = 32.0
 
-    static let genericNetwork              = Image(systemName: "network")
+    static let genericNetwork                = Image(systemName: "network")
 
-    static let evChargerName               = "bolt.circle.fill" // "ev.charger.fill"
-    static let evCharger                   = Image(systemName: evChargerName)
+    static let evChargerName                 = "bolt.circle.fill" // "ev.charger.fill"
+    static let evCharger                     = Image(systemName: evChargerName)
 
-    static let yesName                     = "checkmark.circle"
-    static let yesSymbol                   = Image(systemName: yesName)
-    static let noName                      = "xmark.circle"
-    static let noSymbol                    = Image(systemName: noName)
+    static let yesName                       = "checkmark.circle"
+    static let yesSymbol                     = Image(systemName: yesName)
+    static let noName                        = "xmark.circle"
+    static let noSymbol                      = Image(systemName: noName)
 
     /// Navigation Link Symbols
-    static let accessSymbol                = Image(systemName: "parkingsign.circle")
-    static let connectorSymbol             = Image(systemName: "ev.plug.ac.type.2")
-    static let locationSymbol              = Image(systemName: "mappin.and.ellipse.circle")
-    static let networkSymbol               = Image(systemName: "network")
-    static let chargerSymbol               = Image(systemName: "bolt.circle")
-    static let paymentSymbol               = Image(systemName: "sterlingsign.circle")
+    static let accessSymbol                  = Image(systemName: "parkingsign.circle")
+    static let connectorSymbol               = Image(systemName: "ev.plug.ac.type.2")
+    static let locationSymbol                = Image(systemName: "mappin.and.ellipse.circle")
+    static let networkSymbol                 = Image(systemName: "network")
+    static let chargerSymbol                 = Image(systemName: "bolt.circle")
+    static let paymentSymbol                 = Image(systemName: "sterlingsign.circle")
 
     /// Map Symbols
-    static let searchSymbol                = Image(systemName: "location.magnifyingglass")
-    static let noRecentSearchesSymbolName  = "ev.charger.slash"
-    static let noRecentSearchesSymbol      = Image(systemName: noRecentSearchesSymbolName)
+    static let searchSymbol                  = Image(systemName: "location.magnifyingglass")
+    static let chargerSearchSymbolName       = "ev.charger"
+    static let chargerSearchSymbol           = Image(systemName: chargerSearchSymbolName)
+    static let noRecentSearchesSymbolName    = "ev.charger.slash"
+    static let noRecentSearchesSymbol        = Image(systemName: noRecentSearchesSymbolName)
 
     /// User Settings Symbols
-    static let unitSymbolName              = "ruler"
-    static let unitSymbol                  = Image(systemName: unitSymbolName)
-    static let countrySymbolName           = "map"
-    static let countrySymbol               = Image(systemName: countrySymbolName)
+    static let unitSymbolName                = "ruler"
+    static let unitSymbol                    = Image(systemName: unitSymbolName)
+    static let countrySymbolName             = "map"
+    static let countrySymbol                 = Image(systemName: countrySymbolName)
 }
