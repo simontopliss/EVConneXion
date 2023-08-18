b2590a4cd5c8c779e7c99828da7246b210280be8
simontopliss <simontopliss@me.com>
2023-08-18 21:11:33 +0100

    Rename Model to Models

--- EVChargePoints/Base/Views/SearchToolbarItem.swift
+++ EVChargePoints/Base/Views/SearchToolbarItem.swift
@@ -1,5 +1,5 @@
 //
-//  MapToolbarItem.swift
+//  SearchToolbarItem.swift
 //  EVChargePoints
 //
 //  Created by Simon Topliss on 13/08/2023.
@@ -7,23 +7,23 @@
 
 import SwiftUI
 
-struct MapToolbarItem: View {
+struct SearchToolbarItem: View {
 
     @Binding var showDetails: Bool
     @Binding var showSearch: Bool
+
+    let delay = 0.1
 
     var body: some View {
         Button {
             withAnimation(.snappy) {
                 showDetails = false
             }
-            // NOTE: These seem to cause the "The compiler is unable to type-check this expression in reasonable time; try breaking up the expression into distinct sub-expressions" bug
-            // DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-            // try await Task.sleep(nanoseconds: delay) { // Swift 5.5 version
-            withAnimation(.snappy) {
-                showSearch.toggle()
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                withAnimation(.snappy) {
+                    showSearch.toggle()
+                }
             }
-            // }
         } label: {
             Symbols.searchSymbol
                 .font(.title2)
@@ -33,7 +33,7 @@ struct MapToolbarItem: View {
 }
 
 #Preview {
-    MapToolbarItem(
+    SearchToolbarItem(
         showDetails: .constant(true),
         showSearch: .constant(true)
     )
