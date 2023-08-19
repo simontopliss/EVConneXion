4915231e755c75828ec07f33d9de58e5a6d7d751
simontopliss <simontopliss@me.com>
2023-08-19 21:40:38 +0100

    Change DataManager+Search funcs to throwing

--- EVConneXion/Base/Managers/Data Manager/DataManager+Search.swift
+++ EVConneXion/Base/Managers/Data Manager/DataManager+Search.swift
@@ -10,14 +10,16 @@ import Foundation
 extension DataManager {
 
     @MainActor
-    func searchForChargeDevices(searchQuery: String) async {
+    func searchForChargeDevices(searchQuery: String) async throws {
         print(#function)
         
-        searchError = false
-        searchErrorMessage = ""
         var searchQuery = searchQuery.trim()
 
-        if !isSearchQueryValid(searchQuery: searchQuery) { return }
+        do {
+            if try !isSearchQueryValid(searchQuery: searchQuery) { return }
+        } catch {
+            print(error)
+        }
 
         isPostcode(postcode: searchQuery)
         ? await fetchChargeDevices(requestType: .postcode(searchQuery.localizedUppercase))
@@ -39,19 +41,39 @@ extension DataManager {
     }
 
     /*
+     This version checks the postcode pattern, not just the format
+     See: https://stackoverflow.com/questions/164979/regex-for-matching-uk-postcodes
+     /^(([A-Z]{1,2}\d[A-Z\d]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?\d[A-Z]{2}|BFPO ?\d{1,4}|(KY\d|MSR|VG|AI)[ -]?\d{4}|[A-Z]{2} ?\d{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/
+     */
+
+    /*
      This version will also accept a postcode without a space between the two parts
      /^(([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z])))) ?[0-9][A-Z]{2}$/
      */
 
-    func isSearchQueryValid(searchQuery: String) -> Bool {
+    func isSearchQueryValid(searchQuery: String) throws -> Bool {
         if searchQuery.isEmpty { return false }
         if searchQuery.count < 3 { // Is this min correct?
-            searchError = true
-            searchErrorMessage = "Search query should be more than two characters."
-            return false
+            throw SearchError.invalidPostcode
         }
 
         return true
     }
 
+    enum SearchError: LocalizedError {
+        case invalidPostcode
+        case noResults
+        case custom(error: Error)
+
+        var errorDescription: String? {
+            switch self {
+                case .invalidPostcode:
+                    return "Invalid postcode"
+                case .noResults:
+                    return "No results"
+                case let .custom(error: error):
+                    return "Something went wrong;  \(error.localizedDescription)"
+            }
+        }
+    }
 }
