0ca569c6e425312bff45f9ba359d1a9463b50270
simontopliss <simontopliss@me.com>
2023-09-08 00:31:49 +0100

    Delete separate Tabs file and move to EVConneXion.swift

--- EVConneXion/EVConneXion.swift
+++ EVConneXion/EVConneXion.swift
@@ -110,4 +110,39 @@ extension EVConneXionApp {
         case userSettings    = "UserSettings"
         case recentSearches  = "RecentSearches"
     }
+
+    enum Tabs: Int {
+
+        case map
+        case list
+        case filters
+        case settings
+
+        var label: String {
+            switch self {
+                case .map:
+                    return "Map"
+                case .list:
+                    return "List"
+                case .filters:
+                    return "Filters"
+                case .settings:
+                    return "Settings"
+            }
+        }
+
+        var icon: String {
+            switch self {
+                case .map:
+                    return "map"
+                case .list:
+                    return "list.bullet"
+                case .filters:
+                    return "slider.horizontal.3"
+                case .settings:
+                    return "gearshape"
+            }
+        }
+    }
 }
+
